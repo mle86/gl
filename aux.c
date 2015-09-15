4291d28dd51a6f05f209ac02a9ddf62467a43627
mle <maximilian@eul.cc>
2015-09-15 15:06:39 +0200

    long options

--- aux.c
+++ aux.c
@@ -16,7 +16,9 @@
 */
 
 #define _XOPEN_SOURCE
+#define _GNU_SOURCE
 #include <unistd.h>
+#include <getopt.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -24,8 +26,20 @@
 
 
 void read_arguments (int argc, const char** argv, options_t *o) {
+	const char* short_options = "hVnNpsv";
+	const struct option long_options [] = {
+		{ "help",	0, NULL, 'h' },
+		{ "version",	0, NULL, 'V' },
+		{ "primary",	0, NULL, 'p' },
+		{ "secondary",	0, NULL, 's' },
+		{ "ids",	0, NULL, 'N' },
+		{ "names",	0, NULL, 'n' },
+		{ "verbose",	0, NULL, 'v' },
+		{ 0,0,0,0 }
+	};
+
 	signed char c;
-	while ((c = getopt(argc, (char*const*)argv, "hVnNpsv")) != -1)
+	while ((c = getopt_long(argc, (char*const*)argv, short_options, long_options, NULL)) != -1)
 		switch(c) {
 			case 'h': show_program_help();    exit(0);
 			case 'V': show_program_version(); exit(0);
@@ -41,20 +55,17 @@ void read_arguments (int argc, const char** argv, options_t *o) {
 }
 
 void show_program_help () { printf(
-	M1 "%s" M0"  lists all groups or all members of one group.\n"
-	"Usage: "M1 PROGNAME M0" ["M1"OPTIONS"M0"] ["M1"GROUP"M0"]\n"
+	M1"%s"M0" lists all groups or all members of one group.\n"
+	"Usage: "M1"%s"M0" ["M1"OPTIONS"M0"] ["M1"GROUP"M0"]\n"
 	"Options:\n"
-	M1"  -n    "M0"Print names only instead of names and IDs of groups/members.\n"
-	M1"  -N    "M0"Print IDs only   instead of names and IDs of groups/members.\n"
-	M1"  -p    "M0"Print primary group members only\n"
-	  "        "  "instead of primary and secondary members.\n"
-	M1"  -s    "M0"Print secondary group members only\n"
-	  "        "  "instead of primary and secondary members.\n"
-	M1"  -r    "M0"No linebreaks after each group/member.\n"
-	M1"  -h    "M0"This help.\n"
-	M1"  -V    "M0"Program version information.\n"
+	"  "M1"-n"M0", "M1"--names     "M0"Print group/member names only (default).\n"
+	"  "M1"-N"M0", "M1"--ids       "M0"Print group/member IDs only.\n"
+	"  "M1"-p"M0", "M1"--primary   "M0"Print primary group members only.\n"
+	"  "M1"-s"M0", "M1"--secondary "M0"Print secondary group members only.\n"
+	"  "M1"-h"M0", "M1"--help      "M0"This help.\n"
+	"  "M1"-V"M0", "M1"--version   "M0"Program version information.\n"
 	"\n",
-	PROGNAME
+	PROGNAME, PROGNAME
 ); }
 
 void show_program_version () { printf(
